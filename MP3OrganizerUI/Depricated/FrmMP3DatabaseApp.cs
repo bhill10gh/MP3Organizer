9940ccc291a7880770c85e5f63461f1fdc75d796
bhill10gh <barry.hill@velir.com>
2021-12-14 00:43:23 -0500

    Added cs files

---
+++ MP3OrganizerUI/Depricated/FrmMP3DatabaseApp.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Configuration;
+using System.Collections.Specialized;
+
+using BCHFramework;
+using DAL;
+using MP3OrganizerBusinessLogic;
+
+namespace MP3OrganizerUI
+{
+    public partial class FrmMP3DatabaseApp : Form
+    {
+
+        //public string MP3DBFileName { get; set; }
+
+        #region Events
+
+        private void btnCreatePdbTableFile_Click(object sender, EventArgs e)
+        {
+            DataTable dt = (DataTable)dataGridView1.DataSource;
+            OperationResult op = new OperationResult();
+
+            CsvDb.CreateCsvTable(dt, @"C:\temp\music.txt", ref op, ",", ",");
+            if (!op.Success)
+            {
+                rtbMessages.Text = op.GetAllErrorsAndExceptions("\n");
+                return;
+            }
+        }
+
+        private void btnCreateM3U_Click(object sender, EventArgs e)
+        {
+            OperationResult op = new OperationResult();
+            try
+            {
+                DataTable dt = (DataTable)dataGridView1.DataSource;
+                CreateM3uFromResults(dt, "", ref op);
+                if (!op.Success)
+                {
+                    rtbMessages.Text = op.GetAllErrorsAndExceptions("\n");
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                op.AddException(ex);
+                rtbMessages.Text = op.GetAllErrorsAndExceptions("\n");
+                return;
+            }
+
+            rtbMessages.Text = "Success";
+        }
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+            List<string> list = new List<string>();
+
+            dataGridView1.DataSource = null;
+
+            string wherePart = CreateWherePart(ucatAlbum, "Album");
+            if (wherePart != "'None'" && wherePart.Trim().Length > 0)
+                list.Add(wherePart);
+
+            wherePart = CreateWherePart(ucatArtists, "artist_Name");
+            if (wherePart != "'None'" && wherePart.Trim().Length > 0)
+                list.Add(wherePart);
+
+            wherePart = CreateWherePart(ucatComment, "Comments");
+            if (wherePart != "'None'" && wherePart.Trim().Length > 0)
+                list.Add(wherePart);
+
+            wherePart = CreateWherePart(ucatFileName, "File_Name");
+            if (wherePart != "'None'" && wherePart.Trim().Length > 0)
+                list.Add(wherePart);
+
+            wherePart = CreateWherePart(ucatGenre, "Genre");
+            if (wherePart != "'None'" && wherePart.Trim().Length > 0)
+                list.Add(wherePart);
+
+            wherePart = CreateWherePart(ucatTitle, "Song_Title");
+            if (wherePart != "'None'" && wherePart.Trim().Length > 0)
+                list.Add(wherePart);
+
+            OperationResult op = new OperationResult();
+
+            try
+            {
+                wherePart = MP3DBManager.BuildSearchWhereClause(list, rbAnd.Checked);
+
+
+                MP3DBManager mdbmgr = new MP3DBManager();
+
+                mdbmgr.SetDataStore(ddtbMp3DbFile.ItemText, ref op);
+                Dictionary<string, string> parms = new Dictionary<string, string>();
+                parms.Add("Where_Clause", wherePart);
+
+
+                DataTable dt = mdbmgr.RunSqlScript(ref parms, SqlScriptEnum.GetMP3Data_EnterWhereClause, ref op);
+
+                if (op.Success)
+                {
+                    dataGridView1.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                op.AddException(ex);
+                rtbMessages.Text = op.GetAllErrorsAndExceptions("\n");
+                return;
+            }
+        }
+
+        private void btnRefreshDb_Click(object sender, EventArgs e)
+        {
+            OperationResult op = new OperationResult();
+            ProcessMp3DbFile(ref op);
+        }
+
+        #endregion
+
+
+        #region Methods 
+
+        public FrmMP3DatabaseApp()
+        {
+            InitializeComponent();
+
+            ddtbMp3DbFile.AfterTextDragEvent += AfterFileTextDrop;
+        }
+
+        private void ProcessMp3DbFile(ref  OperationResult op)
+        {
+            try
+            {                
+                if (!File.Exists(ddtbMp3DbFile.ItemText) || !ddtbMp3DbFile.ItemText.ToUpper().EndsWith(".MDB"))
+                {
+                    throw new Exception("You must choose an Access 2000 to 2003 version database (mdb) file!");
+                }
+
+                FillListBoxes(ref op);
+                if (!op.Success)
+                {
+                    rtbMessages.Text = op.GetAllErrorsAndExceptions("\n");
+                    ddtbMp3DbFile.ItemText = string.Empty;
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                op.AddException(ex);
+                rtbMessages.Text = op.GetAllErrorsAndExceptions("\n");
+                return;
+            }
+        }
+
+
+        private void FillListBoxes(ref OperationResult op)
+        {
+            try
+            {
+                MP3DBManager mdbmgr = new MP3DBManager();
+
+                mdbmgr.SetDataStore(ddtbMp3DbFile.ItemText, ref op);
+                Dictionary<string, string> parms = new Dictionary<string, string>();
+
+                string none = "None";
+                DataTable dt = mdbmgr.RunSqlScript(ref parms, SqlScriptEnum.SelectAllAlbums, ref op);
+                ucatAlbum.ClearListbox();
+                ucatAlbum.SetListboxList(dt, "Album");
+                ucatAlbum.InsertListBoxItem(0, none);
+                ucatAlbum.SetSelectedIndex(0);
+
+                dt = mdbmgr.RunSqlScript(ref parms, SqlScriptEnum.SelectAllArtists, ref op);
+                ucatArtists.ClearListbox();
+                ucatArtists.SetListboxList(dt, "Artist_Name");
+                ucatArtists.InsertListBoxItem(0, none);
+                ucatArtists.SetSelectedIndex(0);
+
+                dt = mdbmgr.RunSqlScript(ref parms, SqlScriptEnum.SelectAllComments, ref op);
+                ucatComment.ClearListbox();
+                ucatComment.SetListboxList(dt, "Comments");
+                ucatComment.InsertListBoxItem(0, none);
+                ucatComment.SetSelectedIndex(0);
+
+                dt = mdbmgr.RunSqlScript(ref parms, SqlScriptEnum.SelectAllFileNames, ref op);
+                ucatFileName.ClearListbox();
+                ucatFileName.SetListboxList(dt, "File_Name");
+                ucatFileName.InsertListBoxItem(0, none);
+                ucatFileName.SetSelectedIndex(0);
+
+                dt = mdbmgr.RunSqlScript(ref parms, SqlScriptEnum.SelectAllGenres, ref op);
+                ucatGenre.ClearListbox();
+                ucatGenre.SetListboxList(dt, "Genre");
+                ucatGenre.InsertListBoxItem(0, none);
+                ucatGenre.SetSelectedIndex(0);
+
+                dt = mdbmgr.RunSqlScript(ref parms, SqlScriptEnum.SelectAllSongs, ref op);
+                ucatTitle.ClearListbox();
+                ucatTitle.SetListboxList(dt, "Song_Title");
+                ucatTitle.InsertListBoxItem(0, none);
+                ucatTitle.SetSelectedIndex(0);
+            }
+            catch (Exception ex)
+            {
+                op.AddException(ex);
+                rtbMessages.Text = op.GetAllErrorsAndExceptions("\n");
+                return;
+            }
+        }
+
+        private bool MP3DBExists()
+        {
+            return File.Exists(ddtbMp3DbFile.ItemText);
+        }
+
+        private string CreateWherePart(BCHControls.UCAddToListBox atl, string field)
+        {
+            string wherePart = string.Empty;
+
+            if (atl.SelectedCount < 1)
+            {
+                return wherePart;
+            }
+
+            List<string> list = atl.GetSelectedItems();
+
+            if (atl.SelectedCount == 1 && list[0] == "None")
+            {
+                return wherePart;
+            }
+
+            wherePart = MP3DBManager.BuildSearchWhereClausePart(field, list, true, rbAnd.Checked, cbxUseLike.Checked);
+
+            return wherePart;
+        }
+
+        private void CreateM3uFromResults(DataTable dt, string m3uFileName, ref OperationResult op)
+        {
+
+            try
+            {
+                if (dt == null || dt.Rows.Count < 1)
+                {
+                    return;
+                }
+
+                List<string> songs = new List<string>();
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    songs.Add(Path.Combine((string)row["Path"], (string)row["File_Name"]));
+
+                    BCHFileIO.WriteFullFile(m3uFileName, songs, ref op);
+
+                    if (!op.Success)
+                    {
+                        return;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                op.AddException(ex);
+                return;
+            }
+        }
+
+        private void AfterFileTextDrop(string fName)
+        {
+            OperationResult op = new OperationResult();
+            ProcessMp3DbFile(ref op);
+            
+        }
+
+        #endregion
+
+        
+    }
+
+
+}
---
+++ SqliteDAL/Mp3Repository.cs
@@ -0,0 +1,244 @@
+﻿//using Microsoft.Data.Sqlite;
+using System.Data.SQLite;
+using SqliteDAL.DataContextObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BCHFramework;
+using System.Data;
+using System.Data.Entity;
+
+namespace SqliteDAL
+{
+    public class Mp3TableStatusGroup
+    {
+        public bool HasFileInfoTable { get; set; }
+        public bool HasMp3InfoTable { get; set; }
+        public bool HasArtistTable { get; set; }
+
+        public bool AnyMissngTablds(ref OperationResult op)
+        {
+            if (!HasFileInfoTable || !HasMp3InfoTable || !HasArtistTable)
+            {
+                op.AddInformation("Missing Table)s:\n");
+
+                if (!HasFileInfoTable)
+                {
+                    op.AddInformation("Missing tbFileInfo table.\n");
+                }
+
+                if (!HasMp3InfoTable)
+                {
+                    op.AddInformation("Missing tbMp3Info table.\n");
+                }
+
+                if (!HasArtistTable)
+                {
+                    op.AddInformation("Missing tbArtist table.\n");
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+    public class Mp3Repository
+    {
+        private readonly Mp3DbContext _mp3Context;
+        private readonly string _dbFileName;
+        private readonly SQLiteConnection _sqliteConnection;
+        public Mp3Repository(string dbFileName, ref OperationResult op)
+        {
+            try
+            {
+                _dbFileName = dbFileName;
+                _mp3Context = new Mp3DbContext(dbFileName);
+
+                // Create a new database connection:
+                _sqliteConnection = new SQLiteConnection($"Data Source={dbFileName}; Version = 3; New = True; Compress = True; ");
+            }
+            catch (Exception ex)
+            {
+
+                op.AddException(ex);
+            }
+        }
+
+        public void CreateDatabaseTables(ref OperationResult op)
+        {
+            string createArtistTableSql = "CREATE TABLE [tbArtist]([Artist_Id][INTEGER] PRIMARY KEY, [Mp3Info_Id] [INTEGER] NOT NULL, [Artist_Name] [nvarchar](255) NOT NULL)";
+            string createFileInfoTableSql = "CREATE TABLE [tbFileInfo]([FileInfo_Id][INTEGER] PRIMARY KEY, [File_Name] [nvarchar](250) NOT NULL, [Path] [nvarchar](255) NOT NULL)";
+            string createMp3InfoTableSql = "CREATE TABLE [tbMp3Info]( [Mp3Info_Id] [INTEGER] PRIMARY KEY, [FileInfo_Id] [INTEGER] NOT NULL, [Song_Title] [nvarchar](255) NOT NULL, [Album] [nvarchar](255) NULL, [Genre] [nvarchar](255) NULL, [Comments] [nvarchar](255) NULL, [Track] [INTEGER] NULL, [Song_Numeraton] [nvarchar](255) NULL) ";
+
+            try
+            {
+                _sqliteConnection.Open();
+
+                SQLiteCommand sqlite_cmd;
+                sqlite_cmd = _sqliteConnection.CreateCommand();
+                sqlite_cmd.CommandText = createArtistTableSql;
+                sqlite_cmd.ExecuteNonQuery();
+
+                sqlite_cmd = _sqliteConnection.CreateCommand();
+                sqlite_cmd.CommandText = createFileInfoTableSql;
+                sqlite_cmd.ExecuteNonQuery();
+
+                sqlite_cmd = _sqliteConnection.CreateCommand();
+                sqlite_cmd.CommandText = createMp3InfoTableSql;
+                sqlite_cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+                op.AddException(ex);
+            }
+            finally
+            {
+                _sqliteConnection.Close();
+            }
+        }
+
+        public Mp3TableStatusGroup CheckIfAllTablesExists(ref OperationResult op)
+        {
+            string sql = "SELECT name FROM sqlite_master WHERE type='table';";
+            Mp3TableStatusGroup mp3TableStatusGroup = new Mp3TableStatusGroup();
+
+            try
+            {
+                _sqliteConnection.Open();
+
+                SQLiteCommand sqlite_cmd;
+                sqlite_cmd = _sqliteConnection.CreateCommand();
+                sqlite_cmd.CommandText = sql;
+                sqlite_cmd.ExecuteNonQuery();
+                SQLiteDataAdapter sQLiteDataAdapter = new SQLiteDataAdapter(sqlite_cmd);
+                DataTable dt = new DataTable();
+                sQLiteDataAdapter.Fill(dt);
+
+                if (dt == null || dt.Rows.Count < 1)
+                {
+                    return mp3TableStatusGroup;
+                }
+
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    if (dt.Rows[i][0].ToString() == "tbArtist")
+                    {
+                        mp3TableStatusGroup.HasArtistTable = true;
+                    }
+
+                    if (dt.Rows[i][0].ToString() == "tbFileInfo")
+                    {
+                        mp3TableStatusGroup.HasFileInfoTable = true;
+                    }
+
+                    if (dt.Rows[i][0].ToString() == "tbMp3Info")
+                    {
+                        mp3TableStatusGroup.HasMp3InfoTable = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                op.AddException(ex);
+            }
+            finally
+            {
+                _sqliteConnection.Close();
+            }
+            return mp3TableStatusGroup;
+        }
+
+        public void DropAllTables(ref OperationResult op)
+        {
+            try
+            {
+                _sqliteConnection.Open();
+
+                SQLiteCommand sqlite_cmd;
+
+
+
+                string sql = "DROP TABLE IF EXISTS tbArtist;";
+                sqlite_cmd = _sqliteConnection.CreateCommand();
+                sqlite_cmd.CommandText = sql;
+                sqlite_cmd.ExecuteNonQuery();
+
+                sql = "DROP TABLE IF EXISTS tbMp3Info;";
+                sqlite_cmd = _sqliteConnection.CreateCommand();
+                sqlite_cmd.CommandText = sql;
+                sqlite_cmd.ExecuteNonQuery();
+
+                sql = "DROP TABLE IF EXISTS tbFileInfo;";
+                sqlite_cmd = _sqliteConnection.CreateCommand();
+                sqlite_cmd.CommandText = sql;
+                sqlite_cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                op.AddException(ex);
+            }
+            finally
+            {
+                _sqliteConnection.Close();
+            }
+        }
+
+        public List<tbFileInfo> GetAllMp3FileInfo()
+        {
+            return (from f in _mp3Context.FileInfo select f).ToList();
+        }
+
+        public void InsertMp3(MP3FileDataType mP3FileDataType, ref OperationResult op)
+        {
+            using (DbContextTransaction transaction = _mp3Context.Database.BeginTransaction())
+            {
+                try
+                {
+                    var tbFileInfo = new tbFileInfo
+                    {
+                        File_Name = mP3FileDataType.FileName,
+                        Path = mP3FileDataType.FilePath
+                    };
+                    _mp3Context.FileInfo.Add(tbFileInfo);
+                    _mp3Context.SaveChanges();
+                    var fiId = tbFileInfo.FileInfo_Id;
+
+                    tbMp3Info tbMp3Info = new tbMp3Info
+                    {
+                        Album = mP3FileDataType.Album,
+                        Comments = mP3FileDataType.Comments,
+                        FileInfo_Id = fiId,
+                        Genre = mP3FileDataType.Genre,
+                        Song_Numeraton = mP3FileDataType.SongNumeration,
+                        Song_Title = mP3FileDataType.SongTitle,
+                        Track = mP3FileDataType.Track
+                    };
+                    _mp3Context.Mp3Info.Add(tbMp3Info);
+                    _mp3Context.SaveChanges();
+                    var miId = tbMp3Info.Mp3Info_Id;
+
+                    foreach (var artist in mP3FileDataType.Artists)
+                    {
+                        tbArtist tbArtist = new tbArtist
+                        {
+                            Artist_Name = mP3FileDataType.Album,
+                            Mp3Info_Id = miId
+                        };
+                        _mp3Context.tbArtist.Add(tbArtist);
+                        _mp3Context.SaveChanges();
+                    }
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    op.AddError($"Error inserting mp3 file: {mP3FileDataType.FilePath}");
+                    op.AddException(ex);
+                }
+            }
+        }
+    }
+}
